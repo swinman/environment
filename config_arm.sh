449e00cf65b174e7ef062dc7c5377296fc6a0377
Walker Inman <winman@mit.edu>
2013-11-16 07:12:51 -0500

    add get gtkterm to config arm

--- config_arm.sh
+++ config_arm.sh
@@ -57,6 +57,7 @@ get_packages() {
         sudo apt-get install libusb-0.1-4:i386 -y
         sudo apt-get install lpc21isp -y
         sudo apt-get install openocd -y
+        sudo apt-get install gtkterm -y
         if [ "$(ls /etc/apt/sources.list.d/ | grep "gcc-arm-embedded")" = "" ]; then
             sudo add-apt-repository ppa:terry.guo/gcc-arm-embedded
             sudo apt-get update
