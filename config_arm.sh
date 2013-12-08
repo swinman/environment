81b7b279305cc2b9acb8c34dfe896225b4f340e8
Walker Inman <winman@mit.edu>
2013-12-08 02:11:35 -0500

    add get most recent openocd version

--- config_arm.sh
+++ config_arm.sh
@@ -75,7 +75,6 @@ get_packages() {
     if [ $OS = "linux" ]; then
         sudo apt-get install libusb-0.1-4:i386 -y
         # sudo apt-get install lpc21isp -y
-        sudo apt-get install openocd -y
         sudo apt-get install gtkterm -y
         sudo apt-get install unp -y
         if [ "$(ls /etc/apt/sources.list.d/ | grep "gcc-arm-embedded")" = "" ]; then
@@ -86,9 +85,26 @@ get_packages() {
     fi
 }
 
+get_openocd() {
+    # sudo apt-get install openocd -y
+    if [ $OS = "linux" ]; then
+        sudo apt-get install libtool -y
+        sudo apt-get install autoconf -y
+        sudo apt-get install automake -y
+        sudo apt-get install texinfo -y
+        sudo apt-get install libusb-1.0-0-dev -y
+    fi
+    (cd $toolsdir && git clone git://git.code.sf.net/p/openocd/code openocd)
+    (cd $toolsdir/openocd && ./bootstrap)
+    (cd $toolsdir/openocd && ./configure --enable-stlink --enable-jlink)
+    (cd $toolsdir/openocd && make)
+    (cd $toolsdir/openocd && sudo make install)
+}
+
 
 # --------------------- SETUP SCRIPT --------------------- #
 
 install_tools;
 get_packages;
+get_openocd;
 config_rules;
