417383ec5d4787359e33b026e91bbeb2f7b04565
Walker Inman <winman@mit.edu>
2013-12-09 21:12:47 -0500

    shift config_latex and add 12.04 necessary bkports
    - denote certain repositories as necessary for latex

--- config_latex.sh
+++ config_latex.sh
@@ -7,78 +7,87 @@
 
 # --------------------- DEFINE SEVERAL FUNCTIONS --------------------- #
 config_latex() {
-    if [ "$OS" = "linux" ]; then
-        sudo apt-get install dvipng -y
-        sudo apt-get install xpdf -y
-        sudo apt-get install rubber -y      # compile latex to pdf
-        sudo apt-get install latexmk -y     # similar to rubber
-        sudo apt-get install texlive-latex-base -y
-        sudo apt-get install texlive-latex-extra -y
-        sudo apt-get install texlive-latex-recommended -y
-        sudo apt-get install texlive-plain-extra -y
-        sudo apt-get install texlive-generic-extra -y
-        sudo apt-get install texlive-science -y
+  if [ "$OS" = "linux" ]; then
+    sudo apt-get install dvipng -y
+    sudo apt-get install xpdf -y
+    sudo apt-get install rubber -y    # compile latex to pdf
+    sudo apt-get install latexmk -y   # similar to rubber
+    sudo apt-get install texlive-latex-base -y
+    sudo apt-get install texlive-latex-extra -y
+    sudo apt-get install texlive-latex-recommended -y
+    sudo apt-get install texlive-plain-extra -y
+    sudo apt-get install texlive-generic-extra -y
+    sudo apt-get install texlive-science -y
+    if [ "$(lsb_release -r | sed "s/.*\s\+\(.*\)/\1/")" = "12.04" ]; then
+      echo "Version is 12.04, installing texlive backport"
+      sudo apt-add-repository http://ppa.launchpad.net/texlive-backports/ppa/ubuntu
+      sudo apt-get update
+      sudo apt-get install texlive-base -y
+      sudo apt-get install texlive-xcolor -y
+      sudo apt-get install texlive-latex-extra -y
+      sudo apt-get install texlive-science -y
     fi
+  fi
 }
 
 config_drawing() {
-    if [ "$OS" = "linux" ]; then
-        sudo apt-get install gimp -y
-        sudo apt-get install inkscape -y
-    fi
+  if [ "$OS" = "linux" ]; then
+    sudo apt-get install gimp -y
+    sudo apt-get install inkscape -y
+  fi
 }
 
 get_fonts() {
-    mkdir -p ~/.fonts
-    config_adobe;
-    get_adobe_open_fonts;
-    if [ "$OS" = "linux" ]; then
-        sudo apt-get install lcdf-typetools -y
-        sudo apt-get install ttf-mscorefonts-installer -y
-        sudo apt-get install ttf-oxygen-font-family -y
-        sudo apt-get install texlive-fonts-recommended -y
-        sudo apt-get install texlive-fonts-extra -y
-        sudo apt-get install texlive-font-utils -y
-    fi
+  mkdir -p ~/.fonts
+  config_adobe;
+  get_adobe_open_fonts;
+  if [ "$OS" = "linux" ]; then
+    sudo apt-get install lcdf-typetools -y
+    sudo apt-get install ttf-mscorefonts-installer -y
+    sudo apt-get install ttf-oxygen-font-family -y
+    sudo apt-get install texlive-fonts-recommended -y
+    sudo apt-get install texlive-fonts-extra -y
+    sudo apt-get install texlive-font-utils -y
+  fi
 }
 
 config_adobe() {
-    if [ "$OS" = "linux" ]; then
-        CODENAME=$(lsb_release -a | grep Codename | sed 's/^Codename:\s*//')
-        echo "codename is $CODENAME"
-        sudo add-apt-repository "deb http://archive.canonical.com/ $CODENAME partner"
-        sudo apt-get update
-        sudo apt-get install acroread -y
-        if [ $? -eq 0 ]; then
-            cp /opt/Adobe/Reader9/Resource/Font/*.otf ~/.fonts
-        fi
+  if [ "$OS" = "linux" ]; then
+    CODENAME=$(lsb_release -a | grep Codename | sed 's/^Codename:\s*//')
+    echo "codename is $CODENAME"
+    sudo add-apt-repository "deb http://archive.canonical.com/ $CODENAME partner"
+    sudo apt-get update
+    sudo apt-get install acroread -y
+    if [ $? -eq 0 ]; then
+      cp /opt/Adobe/Reader9/Resource/Font/*.otf ~/.fonts
     fi
+  fi
 }
 
 get_adobe_open_fonts () {
-    FONT_NAME="SourceCodePro"
-    URL="http://sourceforge.net/projects/sourcecodepro.adobe/files/latest/download"
-    FN2="SourceSansPro"
-    URL2="http://sourceforge.net/projects/sourcesans.adobe/files/latest/download?source=files"
+  FONT_NAME="SourceCodePro"
+  URL="http://sourceforge.net/projects/sourcecodepro.adobe/files/latest/download"
+  FN2="SourceSansPro"
+  URL2="http://sourceforge.net/projects/sourcesans.adobe/files/latest/download?source=files"
 
-    mkdir -p /tmp/adodefont
-    cd /tmp/adodefont
-    NEW=false
-    if [ $(ls ~/.fonts | grep $FONT_NAME | wc -l) -eq 0 ]; then
-        wget ${URL} -O ${FONT_NAME}.zip
-        unzip -o -j ${FONT_NAME}.zip
-        mv *.otf ~/.fonts
-        NEW=true
-    fi
-    if [ $(ls ~/.fonts | grep $FN2 | wc -l) -eq 0 ]; then
-        wget ${URL2} -O ${FN2}.zip
-        unzip -o -j ${FN2}.zip
-        mv *.otf ~/.fonts
-        NEW=true
-    fi
-    if [ $NEW = true ]; then
-        fc-cache -f -v
-    fi
+  mkdir -p /tmp/adodefont
+  cd /tmp/adodefont
+  NEW=false
+  if [ $(ls ~/.fonts | grep $FONT_NAME | wc -l) -eq 0 ]; then
+    wget ${URL} -O ${FONT_NAME}.zip
+    unzip -o -j ${FONT_NAME}.zip
+    mv *.otf ~/.fonts
+    NEW=true
+  fi
+  if [ $(ls ~/.fonts | grep $FN2 | wc -l) -eq 0 ]; then
+    wget ${URL2} -O ${FN2}.zip
+    unzip -o -j ${FN2}.zip
+    mv *.otf ~/.fonts
+    NEW=true
+  fi
+  if [ $NEW = true ]; then
+    fc-cache -f -v
+  fi
 }
 
 # for getting fonts:
@@ -90,3 +99,5 @@ config_latex;
 config_drawing;
 get_fonts;
 echo "=============== END: config_latex.sh ==================="
+
+# vim: shiftwidth=2
