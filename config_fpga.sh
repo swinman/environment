9ba1446682fc53c71cfc2fc534eacdd59a94144f
Walker Inman <winman@lucidsci.com>
2014-09-06 09:52:17 -0400

    add -y flag and comment in config fpga script

--- config_fpga.sh
+++ config_fpga.sh
@@ -57,17 +57,18 @@ tejainece_git_script() {
 
     sudo dpkg --add-architecture i386
     sudo apt-get update
-    sudo apt-get install gcc-multilib g++-multilib \
-    lib32z1 lib32stdc++6 lib32gcc1 \
-    expat:i386 fontconfig:i386 libfreetype6:i386 libexpat1:i386 libc6:i386 libgtk-3-0:i386 \
-    libcanberra0:i386 libpng12-0:i386 libice6:i386 libsm6:i386 libncurses5:i386 zlib1g:i386 \
-    libx11-6:i386 libxau6:i386 libxdmcp6:i386 libxext6:i386 libxft2:i386 libxrender1:i386 \
-    libxt6:i386 libxtst6:i386
+    sudo apt-get install -y gcc-multilib g++-multilib lib32z1 lib32stdc++6 \
+    lib32gcc1 expat:i386 fontconfig:i386 libfreetype6:i386 libexpat1:i386 \
+    libc6:i386 libgtk-3-0:i386 libcanberra0:i386 libpng12-0:i386 libice6:i386 \
+    libsm6:i386 libncurses5:i386 zlib1g:i386 libx11-6:i386 libxau6:i386 \
+    libxdmcp6:i386 libxext6:i386 libxft2:i386 libxrender1:i386 libxt6:i386 \
+    libxtst6:i386
     cd /tmp
     wget http://download.savannah.gnu.org/releases/freetype/freetype-2.4.12.tar.bz2
     tar -xjvf freetype-2.4.12.tar.bz2
     cd freetype-2.4.12
-    ./configure --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+    ./configure --build=i686-pc-linux-gnu \
+        "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
     make -j8
 
     mkdir ${ALTERA_PATH}modelsim_ase/lib32
@@ -77,6 +78,7 @@ tejainece_git_script() {
     chmod 755 ${ALTERA_PATH}modelsim_ase/vco
     echo -e "Add the following line \n\texport
     LD_LIBRARY_PATH=\${dir}/lib32\nafter this line\n\tdir=\`dirname \$arg0\`\nin the file ${ALTERA_PATH}modelsim_ase/vco"
+    echo "or dont... it may not be necessary if you use source fconfig"
 }
 
 # --------------------- SETUP SCRIPT --------------------- #
