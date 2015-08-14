55d7902a0364f5d143189b02cc531824d09b54a5
Walker Inman <winman@lucidsci.com>
2015-08-14 08:09:36 -0400

    add ability to run vim_config on subdir

--- vim_config.sh
+++ vim_config.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # use to make ctags, cscope and taghighlight files
+# if you want to provide a root directory for your code
+# that is different from the calling directory, use
+# an additional parameter when you call the script
+
+if [ -n "$1" ]; then
+    SRC_DIR=$(pwd)/$1
+    echo "SRC_DIR set to $SRC_DIR"
+else
+    SRC_DIR=$(pwd)
+fi
 
 if [ "$OS" = "windows" ]; then
     CTAGS=ctags
@@ -11,23 +21,23 @@ fi
 
 if hash cscope 2>/dev/null; then
     echo "Write cscope files list"
-    rm -f cscope.files cscope.out
-    echo "$(find . -name "*.[ch]")" > cscope.files
-    echo "$(find . -name "*.vhd")" >> cscope.files
-    echo "$(find . -name "*.py")" >> cscope.files
+    rm -f $SRC_DIR/cscope.files $SRC_DIR/cscope.out
+    cd $SRC_DIR && echo "$(find . -name "*.[ch]")" > cscope.files
+    cd $SRC_DIR && echo "$(find . -name "*.vhd")" >> cscope.files
+    cd $SRC_DIR && echo "$(find . -name "*.py")" >> cscope.files
 
     echo "Make cscope database"
-    cscope -Rkb -i cscope.files
+    cd $SRC_DIR && cscope -Rkb -i cscope.files
 fi
 
 if hash $CTAGS 2>/dev/null; then
     echo "Make ctags list"
-    rm -f tags
-    $CTAGS -R --exclude="*~" --exclude=".git"
+    rm -f $SRC_DIR/tags
+    cd $SRC_DIR && $CTAGS -R --exclude="*~" --exclude=".git"
 fi
 
 if [ -e $TAGHL ]; then
     echo "Make taghighlight files"
-    rm -f types_*.taghl
-    python $TAGHL --ctags-file tags --source-root .
+    rm -f $SRC_DIR/types_*.taghl
+    cd $SRC_DIR && python $TAGHL --ctags-file tags --source-root .
 fi
