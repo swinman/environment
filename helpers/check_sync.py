504c984731d1ed8394e3ea63f94e93e257264de2
Walker Inman <winman@lucidsci.com>
2015-12-21 14:00:04 -0500

    add size / modify date check on backup

--- helpers/check_sync.py
+++ helpers/check_sync.py
@@ -1,10 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/ipython3 -i
 import os
 
 def walkdrive( drive_top, backup_top ):
 
     drive_top = drive_top.rstrip('/\\')
     backup_top = backup_top.rstrip('/\\')
+
+    if not os.path.exists( drive_top ):
+        print( "Drive not found {}".format( drive_top ) )
+        return
+
+    if not os.path.exists( backup_top ):
+        print( "Backup not found {}".format( backup_top ) )
+        return
 
     def backup_path( drive_path ):
         return drive_path.replace( drive_top, backup_top )
@@ -27,8 +35,19 @@ def walkdrive( drive_top, backup_top ):
             backup = backup_path( path )
             if not os.path.exists( backup ):
                 missing += 1
-                print( "MISSING DIR : {}".format( path ) )
+                print( "MISSING FILE: {}".format( path ) )
                 #print( "    searched  {}".format( backup ) )
+                continue
+            st_base = os.stat( path )
+            st_backup = os.stat( backup )
+            if st_base.st_size != st_backup.st_size:
+                print( "SIZE ERR    : {}".format( path ) )
+                print( "    original {:.2f}mb vs backup {:.2f}mb".format(
+                    st_base.st_size * 1e-6, st_backup.st_size * 1e-6 ) )
+            elif st_base.st_mtime > st_backup.st_mtime + 1:
+                print( "MODIFY ERR  : {}".format( path ) )
+                print( "    original modified {:.0f}s since backup".format(
+                    st_base.st_mtime - st_backup.st_mtime ) )
         if False and not missing:
             print( "Found everything in {}".format( base ) )
             print( "                 in {}".format( base_backup ) )
