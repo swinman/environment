17fc6034a81ee987d3e8a34b1aee0e470c53d4eb
Walker Inman <winman@lucidsci.com>
2016-02-08 10:21:15 -0500

    don't add jedi-vim

--- config_vim.sh
+++ config_vim.sh
@@ -68,7 +68,7 @@ get_vim_addons() {
     get_git_repo $GHURL/tpope/vim-fugitive.git $VBUND/vim-fugitive
     get_git_repo $GHURL/tpope/vim-surround.git $VBUND/vim-surround
     get_git_repo $GHURL/SirVer/ultisnips.git $VBUND/ultisnips
-    get_git_repo $GHURL/davidhalter/jedi-vim.git $VBUND/jedi-vim --recursive
+    #get_git_repo $GHURL/davidhalter/jedi-vim.git $VBUND/jedi-vim --recursive
 
     echo "Attempting to access private repo: "
     get_git_repo git@github.com:swinman/taghighlight.git $VBUND/taghighlight
