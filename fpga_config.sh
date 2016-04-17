82d2384ea8f4458bab83c3cb5111acbc9a6f2b09
Walker Inman <winman@lucidsci.com>
2016-04-17 09:17:15 -0400

    use most recent vsim version

--- fpga_config.sh
+++ fpga_config.sh
@@ -13,14 +13,15 @@ export LM_LICENSE_FILE=$LM_LICENSE_FILE:~/tools/altera/14.0/license.dat
 ICEDIRs=$(find $toolsdir/lscc/iCEcube2* -maxdepth 0 -type d)
 #export ICEDIR=$(echo $ICEDIRs | sed 's/\([^ ]*\).* \(.*\)/\1/')  # first
 export ICEDIR=$(echo $ICEDIRs | sed 's/\([^ ]*\).* \(.*\)/\2/')  # last
-export MSIMDIR=$toolsdir/altera/*/modelsim_ase
+ALTERADIRs=$(find $toolsdir/altera/* -maxdepth 0 -type d)
+ALTERADIR=$(echo $ALTERADIRs | sed 's/\([^ ]*\).* \(.*\)/\2/')
 
 export LD_LIBRARY_PATH=
 export LD_LIBRARY_PATH=$ICEDIR/LSE:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$ICEDIR/LSE/bin/lin:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$ICEDIR/sbt_backend/lib/linux/opt:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$ICEDIR/sbt_backend/bin/linux/opt/synpwrap:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$MSIMDIR/lib32:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$ALTERADIR/modelsim_ase/lib32:$LD_LIBRARY_PATH
 
 export FOUNDRY=$ICEDIR/LSE
 export SYNPLIFY_PATH=$ICEDIR/synpbase
@@ -30,8 +31,8 @@ export DIAMOND_DIR=$toolsdir/lscc/programmer/3.2
 export PATH=$PATH:$DIAMOND_DIR/bin/lin:$DIAMOND_DIR/ispfpga/bin/lin
 export PATH=$PATH:$ICEDIR/synpbase/linux/lib
 
-alias vsim='$toolsdir/altera/*/modelsim_ase/linux/vsim'
-alias quartus='$toolsdir/altera/*/quartus/bin/quartus'
+alias vsim="$ALTERADIR/modelsim_ase/linux/vsim"
+alias quartus="$ALTERADIR/quartus/bin/quartus"
 
 #alias synplify=$ICEDIR/synpbase/linux/mbin/synplify
 #alias synplify_pro=$ICEDIR/synpbase/bin/synplify_pro
