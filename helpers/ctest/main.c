d19971f4a49d235b1921096a2dd63a93e1145b87
Walker Inman <winman@lucidsci.com>
2021-09-29 14:09:49 -0400

    c helper test code

--- helpers/ctest/main.c
+++ helpers/ctest/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
@@ -29,6 +30,60 @@ void make_command(void) {
 
 }
 
+int8_t parse_two_ints_in_str(char* cmd_str) {
+    int ndx, value;
+    char *cmd_nxt, *end_chk;
+
+    if (cmd_str == NULL) {
+        printf("NULL Cmd string, exiting\n");
+        return -1;
+    }
+    while (*cmd_str == ' ') {
+        cmd_str++;
+    }
+
+    if (cmd_str[0] == '\0') {
+        printf("Empty cmd_str, exiting\n");
+        return -2;
+    }
+
+    ndx = strtod(cmd_str, &cmd_nxt);
+    while (*cmd_nxt == ' ') {
+        cmd_nxt++;
+    }
+    if (cmd_nxt[0] == '\0') {
+        printf("Empty value, exiting\n");
+        return -3;
+    }
+
+    value = strtold(cmd_nxt, &end_chk);
+    while (*end_chk == ' ') {
+        end_chk++;
+    }
+    if (*end_chk != '\0') {
+        printf("Invalid data after command '%s', exiting\n", end_chk);
+        return -4;
+    }
+
+    printf("Cmd str is '%s', ndx is %d val is %d\n", cmd_str, ndx, value);
+    return 0;
+}
+
+void test_int_parsing(void) {
+    printf("\n");
+    parse_two_ints_in_str("5 -50");
+    parse_two_ints_in_str(" 5 10");
+    parse_two_ints_in_str(" 5   10");
+    parse_two_ints_in_str(" 5   ");
+    parse_two_ints_in_str(NULL);
+    parse_two_ints_in_str("");
+    parse_two_ints_in_str(" ");
+    parse_two_ints_in_str(" 5 10 4  ");
+    parse_two_ints_in_str("toast 3");
+    parse_two_ints_in_str("3 4a");
+    printf("\n");
+}
+
 int main() {
     mytype_t mb;
     const char *str = NULL;
@@ -39,9 +94,13 @@ int main() {
 
     make_command();
 
+    test_int_parsing();
+
     const char sn[] = "0000-DEAD-BEEF-FFFF";
 
     printf("strlen(sn)=%lu sizeof(sn)=%lu\n", strlen(sn), sizeof(sn));
+
+    printf("print a percent sign: '%%'\n");
 
     printf("Hex rep of 25: 0x%x\n", 25);
     printf("bff.third 0x%x\n", mb.third);
