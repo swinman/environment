d880b7944ba45d631158a4e7cf0184310538be8d
Walker Inman <winman@lucidsci.com>
2023-01-10 19:43:30 -0500

    add default time

--- helpers/tepoc.sh
+++ helpers/tepoc.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 args=$@
->&2 echo "$args ="
+if [ -z $args ]; then
+    args=$(python3 -c "from time import time as t; print(int(t()))")
+fi
 
+>&2 echo "$args ="
 python3 -c "import time as t; print(t.strftime('%Y-%m-%d %I:%M:%S %p', t.localtime($args)))"
